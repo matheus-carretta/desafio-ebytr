6dff04d0db36279c1c7e6771a24ae63434eafd84
Matheus <matheuscarretta@gmail.com>
2022-07-03 17:53:01 -0300

    feat: configura os dados do banco

---
+++ backend/src/db/config/database.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+
+const suffix = {
+  dev: '-dev',
+  development: '-dev',
+  test: '-test',
+};
+
+const options = {
+  host: process.env.HOSTNAME || process.env.MYSQL_HOST || 'localhost',
+  port: process.env.MYSQL_PORT || '3306',
+  database:
+    `${process.env.MYSQL_DB_NAME || suffix.test}`,
+  username: process.env.MYSQL_USER || 'root',
+  password: process.env.MYSQL_PASSWORD || '1234',
+  dialect: 'mysql',
+  dialectOptions: {
+    timezone: 'Z',
+  },
+  logging: process.env.DEBUG !== 'false',
+};
+
+module.exports = {
+  development: {
+    ...options,
+  },
+  test: {
+    ...options,
+  },
+};
