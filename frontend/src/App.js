4c2b25ad8f2a23971ad5b38eca171ee858d0e83d
Matheus <matheuscarretta@gmail.com>
2022-07-04 18:24:15 -0300

    feat: cria o header e pequena alteração na tabela

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 import Body from './components/Body';
+import Header from './components/Header';
 
 function App() {
   return (
-    <Body />
+    <>
+      <Header />
+      <Body />
+    </>
   );
 }
 
