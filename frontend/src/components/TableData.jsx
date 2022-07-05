567dfeaf7d35a119561a25c29c60cce09324a107
Matheus <matheuscarretta@gmail.com>
2022-07-04 21:30:11 -0300

    feat: estiliza botões de editar e deletar

--- frontend/src/components/TableData.jsx
+++ frontend/src/components/TableData.jsx
@@ -4,8 +4,9 @@ return (
   <tr>
     <td>{description}</td>
     <td>{statuses.name}</td>
-    <td> <button type='button'>Atualizar</button>
-      <button type='button'>Deletar</button>
+    <td>
+      <button type='button' className='update-btn'>Atualizar</button>
+      <button type='button' className='delete-btn'>Deletar</button>
     </td>
   </tr>
 )
